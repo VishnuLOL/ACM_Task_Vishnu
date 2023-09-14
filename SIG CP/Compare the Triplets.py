91cb20e3bdbddf03b123c7c6a0824f27016e913a
VishnuLOL <143033036+VishnuLOL@users.noreply.github.com>
2023-09-15 03:11:44 +0530

    Add files via upload

---
+++ SIG CP/Compare the Triplets.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    a_s=0          #alice's score
+    b_s=0          #bob's score
+    for i in range(len(a)):
+        if a[i]>b[i]:
+            a_s+=1
+        elif a[i]<b[i]:
+            b_s+=1
+            
+    return [a_s,b_s]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
